3cef7c7fb89bf7c4f1a4b95b1d966655236ae671
LeoForte1 <133211100+LeoForte1@users.noreply.github.com>
2024-03-25 09:47:48 +0100

    Update CifraioDICesare.java

--- src/cifrario/CifraioDICesare.java
+++ src/cifrario/CifraioDICesare.java
@@ -2,4 +2,15 @@ package cifrario;
 
 public class CifraioDICesare {
 
+	//metodo per cifrare il testo
+	
+	public static String Cifratura(String testoCifrato, int shift) {
+		StringBuilder s1 = new StringBuilder();
+		for(int i=0; i<testoCifrato.length(); i++) {
+			char c = testoCifrato.charAt(i);
+			if(Character.isLetter(c)) {
+				char charSpostato =
+			}
+		}
+	}
 }
