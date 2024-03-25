753adf64066513cb5608375791552885457be796
MatteMazza <matteo.mazzanti2005@gmail.com>
2024-03-25 10:13:28 +0100

    rtgjihnfivbri

--- src/cifrario/CifrarioDizionario.java
+++ src/cifrario/CifrarioDizionario.java
@@ -1,5 +1,17 @@
 package cifrario;
 
 public class CifrarioDizionario {
+	public static void main(String[]args) {
+		
+		char[]alfabetoMaiuscolo = new char[26];
+		for(int i=0;i<26;i++) {
+			alfabetoMaiuscolo[i]=(char)('A'+i);
+		}
+		char[]alfabetoMinuscolo= new char[26];
+		for(int i=0;i<26;i++) //{
+			alfabetoMinuscolo[] 
+			}
+			
+	}
 
 }
