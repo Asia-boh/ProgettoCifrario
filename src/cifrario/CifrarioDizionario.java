a11d8fd92a2a470b7affdd0618ee28b7442c3b4e
MatteMazza <matteo.mazzanti2005@gmail.com>
2024-03-26 10:20:03 +0100

    hchfgfguj
     n

--- src/cifrario/CifrarioDizionario.java
+++ src/cifrario/CifrarioDizionario.java
@@ -1,17 +1,23 @@
 package cifrario;
 
 public class CifrarioDizionario {
-	public static void main(String[]args) {
-		
-		char[]alfabetoMaiuscolo = new char[26];
-		for(int i=0;i<26;i++) {
-			alfabetoMaiuscolo[i]=(char)('A'+i);
-		}
-		char[]alfabetoMinuscolo= new char[26];
-		for(int i=0;i<26;i++) //{
-			alfabetoMinuscolo[] 
+	
+	// Funzione per cifrare il testo 
+	
+	public static  String Cifratura(String testoCifrario) {
+		StringBuilder s1= new StringBuilder();
+		for(int i=0; i<testoCifrario.length();i++)) {
+			char c = testoCifrario.charAt(i);
+			if (Character.isLetter(c)) {
+			char cahrSpostato= (char)(((int)+ shift-'a')% 26 +'a');
+				s1.append(CifraturaDizionario.get(c)));
+				
+			}else {
+				s1.append(c)
 			}
-			
+		}
+	return cifrato.toString();
 	}
 
-}
+
+
