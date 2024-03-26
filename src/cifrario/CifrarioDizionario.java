021a427fa37a2435f7cb751e5725e9ad960ec14e
MatteMazza <matteo.mazzanti2005@gmail.com>
2024-03-26 10:35:49 +0100

    Update CifrarioDizionario.java
    cgcjhbgh

--- src/cifrario/CifrarioDizionario.java
+++ src/cifrario/CifrarioDizionario.java
@@ -4,16 +4,16 @@ public class CifrarioDizionario {
 	
 	// Funzione per cifrare il testo 
 	
-	public static  String Cifratura(String testoCifrario) {
+	public static  String Cifratura(String ; testoCifrario) {
 		StringBuilder s1= new StringBuilder();
-		for(int i=0; i<testoCifrario.length();i++)) {
+		for(int i=0; i<testoCifrario.length();i++) {
 			char c = testoCifrario.charAt(i);
 			if (Character.isLetter(c)) {
-			char cahrSpostato= (char)(((int)+ shift-'a')% 26 +'a');
-				s1.append(CifraturaDizionario.get(c)));
+			char charSpostato= (char)(((int)+ shift-'a')% 26 +'a');
+				s1.append(CifraturaDizionario.get(c));
 				
 			}else {
-				s1.append(c)
+				s1.append(c);
 			}
 		}
 	return cifrato.toString();
