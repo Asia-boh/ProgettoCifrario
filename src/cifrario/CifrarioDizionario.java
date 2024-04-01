34d9bd25399134bc0df4fe49b25ae1573eb7d63b
MatteMazza <matteo.mazzanti2005@gmail.com>
2024-04-01 18:03:15 +0200

    Update CifrarioDizionario.java

--- src/cifrario/CifrarioDizionario.java
+++ src/cifrario/CifrarioDizionario.java
@@ -1,34 +1,25 @@
 package cifrario;
 
 public class CifrarioDizionario {
-	public static String cripta(String information) {
+	public static   String cripta(String Parolaordine) {
 		String stringCript="";
 		char letter;
-		for(int i=0;i < information.length();i++) {
-			letter=information.charAt(i);
-			if(information.charAt(i) >= 65 && information.charAt(i)<= 90) {
-				letter= (char)(122 -(information.charAt(i)+32 - 97));
+		for(int i=0;i < Parolaordine.length();i++) {
+			letter=Parolaordine.charAt(i);
+			if(Parolaordine.charAt(i) >= 65 && Parolaordine.charAt(i)<= 90) {
+				letter= (char)(122 -(Parolaordine.charAt(i)+32 - 97));
 			}
-			if(information.charAt(i)>= 97 && information.charAt(i)<=122) {
-				letter= (char)(90 - (information.charAt(i)- 32 - 65));
+			if(Parolaordine.charAt(i)>= 97 && Parolaordine.charAt(i)<=122) {
+			letter= (char)(90 - (Parolaordine.charAt(i)- 32 - 65));
 			}
 			stringCript += letter;
 		}
 		return stringCript;
 	}
-	public static String decripta(String information) {
-		return cripta(information);
+	public static String decripta(String Parolaordine) {
+		return cripta(Parolaordine);
 		
 	}
-	public static void main(String args[]) {
-		String information= "Ragazzuoli.... bada come la fuma ma che siehe grulli";
-		System.out.println(CifrarioDizionario.cripta(information));
-		String informationCriptata= CifrarioDizionario.cripta(information);
-		System.out.println(CifrarioDizionario.cripta(informationCriptata));
-	}
 }
+
 	
-	
-
-
-
