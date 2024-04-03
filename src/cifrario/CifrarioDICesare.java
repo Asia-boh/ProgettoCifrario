c832eaa8cb9c940e07f5c201ee3517cb2fb4329b
Asia-boh <163838315+Asia-boh@users.noreply.github.com>
2024-04-03 07:30:36 +0200

    Update CifrarioDICesare.java

--- src/cifrario/CifrarioDICesare.java
+++ src/cifrario/CifrarioDICesare.java
@@ -8,10 +8,10 @@ public class CifrarioDICesare {
 		StringBuilder s1 = new StringBuilder();
 		for(int i=0; i<testoCifrato.length(); i++) {
 			char c = testoCifrato.charAt(i);
-			if(Character.isLetter(c)) {
+			if(Character.isLetter(c)){
 				char charSpostato =(char)(((int)c+shift-'a') % 26 +'a');
 				s1.append(charSpostato);
-			} else {
+			}else {
 				s1.append(c);
 			}
 		}
